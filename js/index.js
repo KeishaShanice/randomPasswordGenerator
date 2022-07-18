64aa70b26c81f192d70f33d5e81f7588d80342cd
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-07-18 17:26:44 -0500

    Added Styling

--- js/index.js
+++ js/index.js
@@ -43,134 +43,86 @@ function generatePassword(){
         if((numbers === "no") && (symbols === "no") ) {
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)]
-                        console.log(randomPasswordTwo)
                     }
                 }  else if ((numbers === "yes") && (symbols === "no") ) {
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else if ((numbers === "yes") && (symbols === "") ) {
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else if ((numbers === "") && (symbols === "no") ) {
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else if((numbers === "no") && (symbols === "yes")){
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else if((numbers === "") && (symbols === "yes")){
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else if((numbers === "no") && (symbols === "")){
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                        console.log(randomPasswordTwo)
                     }
                 } else {
                     for(let i = 1; i <= length; i++){
                         randomPasswordOne += characters[Math.floor(Math.random() * characters.length)];
-                        console.log(randomPasswordOne)
                         randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-                        console.log(randomPasswordTwo)
-            
                     }
-                }
-        // for(let i = 1; i <= length; i++){
-        //     randomPasswordOne += characters[Math.floor(Math.random() * characters.length)];
-    
-        //     randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-        // }
-
-        
+                }       
     } else { //15 characters
         if((numbers === "no") && (symbols === "no") ) {
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)]
-                console.log(randomPasswordTwo)
             }
         } else if ((numbers === "yes") && (symbols === "no") ) {
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                console.log(randomPasswordTwo)
             }
         } else if ((numbers === "") && (symbols === "no") ) {
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                console.log(randomPasswordTwo)
             }
         } else if ((numbers === "yes") && (symbols === "") ) {
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-                console.log(randomPasswordTwo)
             }
         } else if((numbers === "no") && (symbols === "yes")){
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                console.log(randomPasswordTwo)
             }
         } else if((numbers === "") && (symbols === "yes")){
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                console.log(randomPasswordTwo)
             }
         } else if((numbers === "no") && (symbols === "")){
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-                console.log(randomPasswordTwo)
             }
         } else {
             for(let i = 1; i <= 15; i++){
                 randomPasswordOne += characters[Math.floor(Math.random() * characters.length)];
-                console.log(randomPasswordOne)
                 randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-                console.log(randomPasswordTwo)
-    
             }
         }
-        
-        // for(let i = 1; i <= 15; i++){
-        //     randomPasswordOne += characters[Math.floor(Math.random() * characters.length)];
-    
-        //     randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-        // }
-
     }
     passwordOne.textContent = randomPasswordOne;
     passwordTwo.textContent = randomPasswordTwo;
@@ -184,63 +136,3 @@ function clear(){
     document.getElementById('numbers').value = "";
     document.getElementById('symbols').value = "";
 }
-
-//use textbox to select for numbers and symbols
-
-//function to generate random password based on numbers and symbols
-// let length = document.querySelector('#passwordLength').value;
-// let numbers = document.querySelector("#numbers").addEventListener('click', check)
-// let symbols = document.querySelector("#symbols");
-
-// function check(){
-//     let numbers = document.querySelector("#numbers").addEventListener('click', check)
-//     if(numbers.checked === true) {
-//         numbers.checked = false
-//     } else {
-//         numbers.checked = true
-//     }
-//     console.log(numbers.checked)
-// }
-
-
-// function generatePassword(){
-//     console.log("clicked")
-//     let length = document.querySelector('#passwordLength').value;
-
-//     let randomPasswordOne = "";
-//     let randomPasswordTwo = "";
-
-//     if((numbers.checked === false) && (symbols.checked === false) ) {
-//         for(let i = 1; i <= length; i++){
-//             randomPasswordOne += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)];
-//             console.log(randomPasswordOne)
-//             randomPasswordTwo += charactersOnlyCharacters[Math.floor(Math.random() * charactersOnlyCharacters.length)]
-//             console.log(randomPasswordTwo)
-//         }
-//     } else if ((numbers.checked === true) && (symbols.checked === false) ) {
-//         for(let i = 1; i <= length; i++){
-//             randomPasswordOne += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)];
-//             console.log(randomPasswordOne)
-//             randomPasswordTwo += charactersSansSymbols[Math.floor(Math.random() * charactersSansSymbols.length)]
-//             console.log(randomPasswordTwo)
-//         }
-//     } else if((numbers.checked === false) && (symbols.checked === true)){
-//         for(let i = 1; i <= length; i++){
-//             randomPasswordOne += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)];
-//             console.log(randomPasswordOne)
-//             randomPasswordTwo += charactersSansNumbers[Math.floor(Math.random() * charactersSansNumbers.length)]
-//             console.log(randomPasswordTwo)
-//         }
-//     } else {
-//         for(let i = 1; i <= length; i++){
-//             randomPasswordOne += characters[Math.floor(Math.random() * characters.length)];
-//             console.log(randomPasswordOne)
-//             randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
-//             console.log(randomPasswordTwo)
-
-//         }
-//     }
-    
-//     passwordOne.textContent = randomPasswordOne;
-//     passwordTwo.textContent = randomPasswordTwo;
-// }
